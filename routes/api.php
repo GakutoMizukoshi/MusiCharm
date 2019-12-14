ef4d3ac08f2aaecb7fcbd6801b5c30876fa52bd8
GakutoMizukoshi <kiniro.kiniro@gmail.com>
2019-12-14 23:41:21 +0900

    写真投稿API周りのロジック追加

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,6 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 
@@ -25,6 +10,9 @@ Route::post('/login', 'Auth\LoginController@login')->name('login');
 
 // ログアウト
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+// 写真投稿
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
 
 // ログインユーザー取得
 Route::get('/user', function () {
